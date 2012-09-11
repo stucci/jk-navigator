17fceb6d284f83e2bdde5757fed0000b4fc6adf1
Thomas Steinacher <tom@eggdrop.ch>
2012-09-10 23:31:36 -0700

    Fixing #7

--- script.js
+++ script.js
@@ -95,7 +95,7 @@
     }
 
     set_domain_opts(document.domain);
-    
+
     function active_selector(idx) {
         return group_selector.replace(':nth(*)', ':nth('+idx+')')
     }
@@ -119,39 +119,46 @@
 
 
     function start(focusResult) {
-        $(site_opts.selectors).each(function(n, selector) {
-            if (selector instanceof Array) {
-                group_selector_all = selector[0];
-                group_selector = selector[1];
-            } else {
-                group_selector_all = selector.replace(':nth(*)', '');
-                group_selector = selector;
-            }
+        if (!group_selector) {
+            $(site_opts.selectors).each(function(n, selector) {
+                if (selector instanceof Array) {
+                    group_selector_all = selector[0];
+                    group_selector = selector[1];
+                } else {
+                    group_selector_all = selector.replace(':nth(*)', '');
+                    group_selector = selector;
+                }
+                var result_links_container = $(group_selector_all);
+                if (result_links_container.length) {
+                    return false; // break
+                } else {
+                    group_selector = group_selector_all = null;
+                }
+            });
+        }
 
-            var result_links_container = $(group_selector_all);
-            if (result_links_container.length) {
-                //store the links into the local storage
-                result_links = []
-                result_links_container.each(function(m, link) {
-                    result_links.push($(link).attr('href'));
-                });
-                new_result_links = JSON.stringify(result_links);
-                if (localStorage.result_links != new_result_links)
-                {
-                    if (focusResult && localStorage.idx != -1) { localStorage.idx = 0; }
-                    localStorage.result_links = new_result_links;
-                }
-                localStorage.start_page = location.href;
-                if (localStorage.idx == -1) {
-                    localStorage.idx = result_links.length-1;
-                }
-                return false; // break
-            } else {
-                group_selector = group_selector_all = null;
-            }
-        });
         if (!group_selector)
             return;
+
+        var result_links_container = $(group_selector_all);
+        if (result_links_container.length) {
+            //store the links into the local storage
+            result_links = []
+            result_links_container.each(function(m, link) {
+                result_links.push($(link).attr('href'));
+            });
+            new_result_links = JSON.stringify(result_links);
+            if (localStorage.result_links != new_result_links)
+            {
+                if (focusResult && localStorage.idx != -1) { localStorage.idx = 0; }
+                localStorage.result_links = new_result_links;
+            }
+            localStorage.start_page = location.href;
+            if (localStorage.idx == -1) {
+                localStorage.idx = result_links.length-1;
+            }
+        }
+
         var newNode = $(active_selector(localStorage.idx));
         if (!node || (node.attr('href') != newNode.attr('href'))) {
             if (!localStorage.idx) {
@@ -164,7 +171,7 @@
                 node = newNode;
             }
         }
-        
+
     }
 
     var lock = false;
@@ -184,108 +191,108 @@
                 });
             }
         }
-    });
 
-    //define what the different keystrokes do
-    if (site_opts)
-    {
-        key('j', function(ev) {
-            if (group_selector) {
-                if (localStorage.idx < $(group_selector_all).length-1) {
-                    localStorage.idx++;
-                    select(true);
+        //define what the different keystrokes do
+        if (site_opts)
+        {
+            key('j', function(ev) {
+                if (group_selector) {
+                    if (localStorage.idx < $(group_selector_all).length-1) {
+                        localStorage.idx++;
+                        select(true);
+                    }
+                    else {
+                        if (site_opts.paginator_selector_next && $(site_opts.paginator_selector_next).length) {
+                            localStorage.idx = 0;
+                            location.href = $(site_opts.paginator_selector_next).attr('href');
+                        }
+                    }
+                    ev.stopPropagation();
                 }
                 else {
-                    if (site_opts.paginator_selector_next && $(site_opts.paginator_selector_next).length) {
-                        localStorage.idx = 0; 
-                        location.href = $(site_opts.paginator_selector_next).attr('href');
+                    result_links = JSON.parse(localStorage.result_links);
+                    if (link = result_links[++localStorage.idx]) {
+                        location.href = link;
                     }
-                }
-                ev.stopPropagation();
-            }
-            else { 
-                result_links = JSON.parse(localStorage.result_links);
-		        if (link = result_links[++localStorage.idx]) {
-		            location.href = link;		
-		        }
-		        else {
-                   localStorage.idx--;
-		        }
+                    else {
+                       localStorage.idx--;
+                    }
 
-            }
-        });
-        key('k', function(ev) {
-            if (group_selector) {
-                if (localStorage.idx > 0) {
-                    localStorage.idx--;
-                    select(true);
+                }
+            });
+            key('k', function(ev) {
+                if (group_selector) {
+                    if (localStorage.idx > 0) {
+                        localStorage.idx--;
+                        select(true);
+                    }
+                    else {
+                        if (site_opts.paginator_selector_prev && $(site_opts.paginator_selector_prev).length && $(site_opts.paginator_selector_prev).attr('href') != 'javascript:;') {
+                            localStorage.idx = -1;
+                            location.href = $(site_opts.paginator_selector_prev).attr('href');
+                        }
+
+                    }
+                    ev.stopPropagation();
                 }
                 else {
-                    if (site_opts.paginator_selector_prev && $(site_opts.paginator_selector_prev).length && $(site_opts.paginator_selector_prev).attr('href') != 'javascript:;') {
-                        localStorage.idx = -1; 
-                        location.href = $(site_opts.paginator_selector_prev).attr('href');
-                    }
-                     
+                    result_links = JSON.parse(localStorage.result_links);
+                        if (link = result_links[--localStorage.idx]) {
+                            location.href = link;
+                        }
+                        else {
+                       localStorage.idx++;
+                        }
                 }
-                ev.stopPropagation();
-            }
-            else {
-                result_links = JSON.parse(localStorage.result_links);
-		            if (link = result_links[--localStorage.idx]) {
-		                location.href = link;		
-		            }
-		            else {
-                   localStorage.idx++;
-		            }
-            }
-        });
-        key('/', function(ev) {
-            if (site_opts.search_selector)
-                $(site_opts.search_selector).focus();
-            ev.stopPropagation();
-            ev.preventDefault();
-        });
-        function open_link(ev, newWindow, force) {
-            var link = select();
-
-            // If no particular element is focused, open the selected link.
-            // Else, use the browser implementation to open the focused link
-            // which already does the right thing.
-            if (force || ev.target == document.body) {
-                if (newWindow) {
-                    window.open(link.attr('href'));
-                } else {
-                    location.href = link.attr('href');
-                }
+            });
+            key('/', function(ev) {
+                if (site_opts.search_selector)
+                    $(site_opts.search_selector).focus();
                 ev.stopPropagation();
                 ev.preventDefault();
-            }
-        }
+            });
+            function open_link(ev, newWindow, force) {
+                var link = select();
 
-        key('return', function(ev) {
-            open_link(ev, false);
-        });
-    
-        key('⌘+return', function(ev) {
-            open_link(ev, true);
-        });
-
-        key('o', function(ev) {
-            open_link(ev, false, true);
-        });
-
-        key('i', function(ev) {
-            if (group_selector) {
-                if (localStorage.idx > 0) {
-                    localStorage.idx=0;
-                    select(true);
+                // If no particular element is focused, open the selected link.
+                // Else, use the browser implementation to open the focused link
+                // which already does the right thing.
+                if (force || ev.target == document.body) {
+                    if (newWindow) {
+                        window.open(link.attr('href'));
+                    } else {
+                        location.href = link.attr('href');
+                    }
+                    ev.stopPropagation();
+                    ev.preventDefault();
                 }
-                ev.stopPropagation();
             }
-            else {
-                location.href = localStorage.start_page;
-            }
-        });
-    }
+
+            key('return', function(ev) {
+                open_link(ev, false);
+            });
+
+            key('⌘+return', function(ev) {
+                open_link(ev, true);
+            });
+
+            key('o', function(ev) {
+                open_link(ev, false, true);
+            });
+
+            key('i', function(ev) {
+                if (group_selector) {
+                    if (localStorage.idx > 0) {
+                        localStorage.idx=0;
+                        select(true);
+                    }
+                    ev.stopPropagation();
+                }
+                else {
+                    location.href = localStorage.start_page;
+                }
+            });
+        }
+    });
 
 })();
